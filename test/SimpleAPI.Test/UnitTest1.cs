62c362587d529cd9cc1215eeabbb460364a3557e
vinicius <viniciuscoelhonau975@gmail.com>
2020-09-29 16:40:26 -0300

    test project

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("test", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
